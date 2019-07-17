4ddf763f81c6cda41cbfe80f9cb4d8804c1c0f30
paulll <paulll@derpy.ru>
2019-07-17 13:22:47 +0300

    add proof-of-concept code

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import {ProcessorNode, Token, Executor, Context} from "@paulll/siso";
+import {StringAsVkUser} from "./StringAsVkUser";
+import {VkUserMainData} from "./VkUserMainData";
+import {VkUserFoafData} from "./VkUserFoafData";
+import {API} from "@paulll/vklib";
+
+const executor = new Executor();
+executor.addNode(new StringAsVkUser());
+executor.addNode(new VkUserMainData());
+executor.addNode(new VkUserFoafData());
+
+const ctx = executor.createContext({vkApi: new API({
+		service_token: "c8dbb40ac8dbb40ac8dbb40a64c8ebf155cc8dbc8dbb40a92df9d61ae3e1068cf1fa8e9",
+		access_token: false
+})});
+
+(async () => {
+	await ctx.run([
+		new Token(["string"], "vk: 281843929", 1),
+	]);
+})();
+
+
+ctx.on("newToken", async (token: Token) => {
+	//console.log(token);
+	console.dir(token, { depth: null });
+});
