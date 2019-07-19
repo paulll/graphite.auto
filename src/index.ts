33c3e89703cafecb5b6a08f4038338a3f1e84f2b
paulll <paulll@derpy.ru>
2019-07-19 14:12:59 +0300

    add VkUserWall node

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import {VkUserMainData} from "./VkUserMainData";
 import {VkUserFoafData} from "./VkUserFoafData";
 import {ErrorReporter} from "./ErrorReporter";
 import {VkUserAvatars} from "./VkUserAvatars";
+import {VkUserWall} from "./VkUserWall";
 
 const executor = new Executor();
 
@@ -14,6 +15,7 @@ executor.addNode(new VkUserMainData());
 executor.addNode(new VkUserFoafData());
 executor.addNode(new ErrorReporter());
 executor.addNode(new VkUserAvatars());
+executor.addNode(new VkUserWall());
 
 const ctx = executor.createContext({vkApi: new API({
 		service_token: "c8dbb40ac8dbb40ac8dbb40a64c8ebf155cc8dbc8dbb40a92df9d61ae3e1068cf1fa8e9",
